eaf3a9ae97c3e99f9c68d589c4ea52963241de86
Satyam <satyamkt512@gmail.com>
2021-07-18 18:36:12 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+var items=["submit the feedback form","Complete the remaining assignments"];                                       //empty array=>empty to-do list
+
+app.get("/", function (req, res) {
+    var today = new Date();
+
+    var options = { weekday: 'long', month: 'long', day: 'numeric' };
+
+    var day = today.toLocaleDateString("en-us", options);
+
+    res.render("list", { day: day ,items:items});
+});
+
+
+
+app.post("/", function (req, res) {
+    if(req.body.newItem!="")
+    items.push(req.body.newItem);
+    res.redirect("/");
+});
+
+app.listen(3000, function () {
+    console.log("server is running on port 3000");
+});
